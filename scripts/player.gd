ff9b4063593370e27292c585fa399900c61b6570
Ogster08 <104634399+Ogster08@users.noreply.github.com>
2024-05-07 22:46:49 +0100

    added acceleration to player

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
-const SIZE = 4
-const SPEED = 20.0 * SIZE
-const JUMP_VELOCITY = -20.0 * SIZE
-# set gravity to 192 * size
+const MAX_SPEED = 240
+const ACCELERATION = 100
+const JUMP_VELOCITY = -240
+# set gravity to 50 * size
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -21,8 +21,8 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	var direction = Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x =  clamp(velocity.x + direction * ACCELERATION, -MAX_SPEED, MAX_SPEED)
 	else:
-		velocity.x = move_toward(velocity.x, velocity.x * 0.1, SPEED)
+		velocity.x = move_toward(velocity.x, 0, ACCELERATION)
 
 	move_and_slide()
