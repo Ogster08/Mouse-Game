47e638c66d2b791d198f2be8bcf3a69032da21a7
Ogster08 <104634399+Ogster08@users.noreply.github.com>
2024-05-07 21:43:13 +0100

    experiment with lighting

---
+++ scripts/player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+const SIZE = 4
+const SPEED = 20.0 * SIZE
+const JUMP_VELOCITY = -20.0 * SIZE
+# set gravity to 192 * size
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, velocity.x * 0.1, SPEED)
+
+	move_and_slide()
